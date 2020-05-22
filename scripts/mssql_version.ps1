607287198da1459df845ddeae81b71bf39ee8a5f
ltjessem <ltjessem@gmail.com>
2020-05-22 13:50:00 +0200

    Get instance version information from registry

--- scripts/mssql_version.ps1
+++ scripts/mssql_version.ps1
@@ -1,3 +1,6 @@
-﻿$ver = Invoke-Sqlcmd -Query "SELECT @@VERSION;" -QueryTimeout 3
-
-write-host $ver.Column1
+$inst = (get-itemproperty 'HKLM:\SOFTWARE\Microsoft\Microsoft SQL Server').InstalledInstances
+foreach ($i in $inst)
+{
+   $p = (Get-ItemProperty 'HKLM:\SOFTWARE\Microsoft\Microsoft SQL Server\Instance Names\SQL').$i
+   Write-host $i (Get-ItemProperty "HKLM:\SOFTWARE\Microsoft\Microsoft SQL Server\$p\Setup").PatchLevel
+}
